1526b7bbd221edd9f2d3fa7c8fc8a3c9d65da630
Hertzian <tallitosan@gmail.com>
2022-03-14 17:09:18 -0500

    first commit

---
+++ 1_schemasAndQueries/src/index.js
@@ -0,0 +1,11 @@
+import sumeichon, { subtract } from './math'
+import myCurrentLocation, { getGreeting, message, name } from './myModule'
+//import myCurrentLocation from './myModule' //valid as well
+
+console.log(message)
+console.log(name)
+console.log(myCurrentLocation)
+console.log(getGreeting(name))
+console.log(sumeichon(1, -2))
+console.log(subtract(10, 2))
+
