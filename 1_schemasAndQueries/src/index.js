b7bfca09261edd329f8e82190b4f2f974c536cb5
Hertzian <tallitosan@gmail.com>
2022-03-15 16:12:33 -0500

    custom types and dev command in package.json

--- 1_schemasAndQueries/src/index.js
+++ 1_schemasAndQueries/src/index.js
@@ -1,11 +1,64 @@
-import sumeichon, { subtract } from './math'
-import myCurrentLocation, { getGreeting, message, name } from './myModule'
-//import myCurrentLocation from './myModule' //valid as well
+//import { GraphQLServer } from 'graphql-yoga'
+const { GraphQLServer } = require('graphql-yoga')
+//datatypes for graphql
+//strings, booleans, int, float, ID -> to use in unique identifiers
 
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreeting(name))
-console.log(sumeichon(1, -2))
-console.log(subtract(10, 2))
+//type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    id: ID!
+    name: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+    me: User!
+    post: Post!
+  }
 
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+`
+
+//resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'My first query',
+    id: () => 'asdf1234',
+    name: () => 'Lalitus',
+    age: () => '38',
+    employed: () => true,
+    gpa: () => null,
+    me: () => {
+      return {
+        id: 'asdflkj',
+        name: 'lalo',
+        email: 'cawn@email.com',
+        age: '23'
+      }
+    },
+    post: () => {
+      return {
+        id: 'dsñflkj234',
+        title: 'this is a post',
+        body: 'this is the body',
+        published: true
+      }
+    }
+  }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.start(() => console.log(`The server is up in http://localhost:4000`))
